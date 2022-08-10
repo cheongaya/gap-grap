34b098dc53e1a03ab6a61aa2de8190da679bcfb7
cheongaya <chengalongo@gmail.com>
2022-08-10 20:28:31 +0900

    Add : 어노테이션 추가

--- src/main/java/com/roovalant/gapgrap/GapGrapApplication.java
+++ src/main/java/com/roovalant/gapgrap/GapGrapApplication.java
@@ -1,8 +1,15 @@
 package com.roovalant.gapgrap;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@AllArgsConstructor
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class GapGrapApplication {
 
