2621062cd3e26bb812a25adb35b7665315a215bf
cheongaya <chengalongo@gmail.com>
2022-08-10 20:03:03 +0900

    Initial commit

---
+++ src/main/java/com/roovalant/gapgrap/GapGrapApplication.java
@@ -0,0 +1,13 @@
+package com.roovalant.gapgrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapGrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GapGrapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roovalant/gapgrap/GapGrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roovalant.gapgrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapGrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
