fe2e3fe5e59c937121b84eb5c9ee15109e50b3ad
cheongaya <chengalongo@gmail.com>
2022-08-29 23:02:28 +0900

    Add : Apt Entity 필드 추가

--- src/main/java/com/roovalant/gapgrap/domain/Apt.java
+++ src/main/java/com/roovalant/gapgrap/domain/Apt.java
@@ -3,10 +3,14 @@ package com.roovalant.gapgrap.domain;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+
 /**
  * The Apt entity
  *
@@ -19,5 +23,29 @@ import java.io.Serializable;
 @EqualsAndHashCode(of = "id")
 @Table(name = "apt")
 public class Apt implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id: 아파트 식별자
+     */
+    @Id
+    @NotNull
+    @Size(max = 40)
+    @Column(name = "id", length = 40, unique = true, nullable = false)
+    private String id;
+
+    /**
+     * name: 이름
+     */
+    @Size(max = 255)
+    @Column(name = "name", length = 255)
+    private String name;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+
 
 }
