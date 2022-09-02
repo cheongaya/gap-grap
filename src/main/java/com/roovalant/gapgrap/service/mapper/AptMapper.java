fee96ea4776accab479a02351e9efd2e055cfcbc
cheongaya <chengalongo@gmail.com>
2022-09-02 20:57:29 +0900

    Add : Apt mapper 클래스 생성

---
+++ src/main/java/com/roovalant/gapgrap/service/mapper/AptMapper.java
@@ -0,0 +1,22 @@
+package com.roovalant.gapgrap.service.mapper;
+
+import com.roovalant.gapgrap.domain.Apt;
+import com.roovalant.gapgrap.service.dto.AptDTO;
+
+@Mapper(componentModel = "spring", uses = {
+}, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public class AptMapper extends EntityMapper<AptDTO, Apt> {
+
+    AptDTO toDto(Apt entity);
+
+    Apt toEntity(AptDTO dto);
+
+    default Apt fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Apt o = new Apt();
+        o.setId(id);
+        return o;
+    }
+}
