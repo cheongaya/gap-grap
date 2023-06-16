489b0b331d72b5efb4129d549cdce076276b41aa
cheonga-code <chengalongo@gmail.com>
2023-06-17 01:22:57 +0900

    add : Dollar 대신 Franc 클래스 추가 후 테스트 코드 실행

---
+++ src/main/java/com/roovalant/gapgrap/tdd/money/Franc.java
@@ -0,0 +1,18 @@
+package com.roovalant.gapgrap.tdd.money;
+
+public class Franc {
+    private int amount;
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
+}
