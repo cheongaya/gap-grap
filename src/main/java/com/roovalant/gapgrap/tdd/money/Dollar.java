716351295e7c031557d57be308e73bcc22e7c0bb
cheonga-code <chengalongo@gmail.com>
2023-06-17 00:39:36 +0900

    fix : 중복 제거를 위해 *= 연산자 사용

--- src/main/java/com/roovalant/gapgrap/tdd/money/Dollar.java
+++ src/main/java/com/roovalant/gapgrap/tdd/money/Dollar.java
@@ -8,6 +8,7 @@ public class Dollar {
 
     void times(int multiplier) {
 //        amount = 5 * 2;
-        amount = amount * multiplier; // 원래는 5 * 2 => 중복제거 (리팩토링)
+//        amount = amount * multiplier; // 원래는 5 * 2 => 중복제거 (리팩토링)
+        amount *= multiplier;
     }
 }
