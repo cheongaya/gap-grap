b7749ca4baec6e0c89d364cd8ac66992120aa483
cheonga-code <chengalongo@gmail.com>
2023-06-17 00:54:21 +0900

    fix : Dollar 부작용 해결을 위해 코드 수정 (초록막대)

--- src/main/java/com/roovalant/gapgrap/tdd/money/Dollar.java
+++ src/main/java/com/roovalant/gapgrap/tdd/money/Dollar.java
@@ -9,7 +9,10 @@ public class Dollar {
     Dollar times(int multiplier) {
 //        amount = 5 * 2;
 //        amount = amount * multiplier; // 원래는 5 * 2 => 중복제거 (리팩토링)
-        amount *= multiplier;
-        return null;
+
+//        amount *= multiplier;
+//        return null;
+
+        return new Dollar(amount * multiplier);
     }
 }
