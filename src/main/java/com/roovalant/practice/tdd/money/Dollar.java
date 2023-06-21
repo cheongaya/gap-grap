f591bdfbd4b2cb745ee896451c3a74cd4cbb5e09
cheonga-code <chengalongo@gmail.com>
2023-06-21 17:45:22 +0900

    del : 하위 클래스(Dollar, Franc) 는 이제 참조하는 곳이 없으므로 필요 없어져서 삭제 (삭제 대신 주석처리함)

--- src/main/java/com/roovalant/practice/tdd/money/Dollar.java
+++ src/main/java/com/roovalant/practice/tdd/money/Dollar.java
@@ -1,25 +1,25 @@
-package com.roovalant.practice.tdd.money;
-
-public class Dollar extends Money {
-
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-
-//    Dollar times(int multiplier) {
-////        amount = 5 * 2;
-////        amount = amount * multiplier; // 원래는 5 * 2 => 중복제거 (리팩토링)
+//package com.roovalant.practice.tdd.money;
 //
-////        amount *= multiplier;
-////        return null;
+//public class Dollar extends Money {
 //
-//        return new Dollar(amount * multiplier);
+//    Dollar(int amount, String currency) {
+//        super(amount, currency);
 //    }
-
-//    @Override
-//    public boolean equals(Object obj) {
-////        return true;
-//        Dollar dollar = (Dollar) obj;
-//        return amount == dollar.amount;
-//    }
-}
+//
+////    Dollar times(int multiplier) {
+//////        amount = 5 * 2;
+//////        amount = amount * multiplier; // 원래는 5 * 2 => 중복제거 (리팩토링)
+////
+//////        amount *= multiplier;
+//////        return null;
+////
+////        return new Dollar(amount * multiplier);
+////    }
+//
+////    @Override
+////    public boolean equals(Object obj) {
+//////        return true;
+////        Dollar dollar = (Dollar) obj;
+////        return amount == dollar.amount;
+////    }
+//}
--- src/main/java/com/roovalant/practice/tdd/money/Franc.java
+++ src/main/java/com/roovalant/practice/tdd/money/Franc.java
@@ -1,18 +1,18 @@
-package com.roovalant.practice.tdd.money;
-
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-
-//    Franc times(int multiplier) {
-//        return new Franc(amount * multiplier);
+//package com.roovalant.practice.tdd.money;
+//
+//public class Franc extends Money {
+//
+//    Franc(int amount, String currency) {
+//        super(amount, currency);
 //    }
-
-//    @Override
-//    public boolean equals(Object obj) {
-//        Franc franc = (Franc) obj;
-//        return amount == franc.amount;
-//    }
-}
+//
+////    Franc times(int multiplier) {
+////        return new Franc(amount * multiplier);
+////    }
+//
+////    @Override
+////    public boolean equals(Object obj) {
+////        Franc franc = (Franc) obj;
+////        return amount == franc.amount;
+////    }
+//}
